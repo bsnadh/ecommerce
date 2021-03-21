0485b8be29266518c9f44914f55d94290cc6de26
bishnu <bsnadh984@gmail.com>
2021-03-21 16:58:28 +0545

    day 2

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        $photos = \App\Models\Photo::all();
+        
+        return view('site.pages.home')->with(['photos' => $photos]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PhotoController;
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\CheckoutController;
+use App\Http\Controllers\PaymentController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,8 +17,27 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/',[HomeController::class, 'index']);
+Route::get('/',[HomeController::class, 'index']);
 
-Route::view('/', 'site.pages.home')->name('home');
+//Photo Routes
+Route::get("/photo/create", [PhotoController::class, 'create']);
+Route::post("/photo/store", [PhotoController::class, 'store']);
+Route::get("/photo/{id}", [PhotoController::class, 'show']);
+
+//cart routes
+Route::post("/cart/save", [CartController::class, 'save']);
+Route::get("/cart/summary", [CartController::class, 'summary']);
+Route::delete('/cart/item/{id}', [CartController::class, 'destroy']);
+
+//checkout routes
+Route::get('/checkout',[CheckoutController::class, 'index']);
+Route::post('/checkout',[CheckoutController::class, 'saveEmail']);
+
+//payment route
+Route::get('/payment',[PaymentController::class, 'index']);
+
+// Route::view('/', 'site.pages.home')->name('home');
 Route::view('/photo/show', 'site.pages.show-photo');
 Route::view('/cart', 'site.pages.cart-summary');
 Route::view('/panel/downloads', 'site.pages.panel.downloads');
